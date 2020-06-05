dc9a5dd32163c2dbd1750663d7b8edf1384e82ec
portsoft3710 <47545184+portsoft3710@users.noreply.github.com>
2020-06-05 16:54:38 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,17 @@
 // service-worker.js
-self.addEventListener('install', function(e) {
-  console.log('[ServiceWorker] Install');
+var CACHE_NAME = 'janken-cache-2020060401';
+var urlsToCache = [
+    'index.html'
+];
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache.map(url => new Request(url, {credentials: 'same-origin'})));
+      })
+  );
 });
 
 self.addEventListener('activate', function(e) {
@@ -9,3 +20,17 @@ self.addEventListener('activate', function(e) {
 
 // 現状では、この処理を書かないとService Workerが有効と判定されないようです
 self.addEventListener('fetch', function(event) {});
+
+self.addEventListener('push', function (event) {
+    console.log('Received a push message', event);
+    var title = "プッシュ通知です！";
+    var body = "プッシュ通知はこのようにして送られるのです";
+
+    event.waitUntil(
+        self.registration.showNotification(title, {
+            body: body,
+            icon: '/images/icons/icon-152x152.png',
+            tag: 'push-notification-tag'
+        })
+    );
+});
